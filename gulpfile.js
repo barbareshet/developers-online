20e7fa9733743d7c9164a2efe925c9210852b7f7
Ido <barbareshet@gmail.com>
2016-09-16 09:04:37 +0300

    test 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue');
 
@@ -13,7 +13,13 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir(mix => {
+/*elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
+});*/
+elixir(function(mix) {
+    mix.sass([
+        'app.scss',
+        'controllers.scss'
+    ], 'public/assets/css');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -4,18 +4,80 @@
  * and open the template in the editor.
  */
 
-$('document').ready(function(){
-    $(window).scroll(function(){
-        
-       $('.left').animate({
+/*$('document').ready(function(){
+    
+    var $animation_elements_left = $('.left');
+     var $animation_elements_right = $('.right');
+    var $window = $(window);
+    $window.scrollTop(function(){
+        if($(this).height() >= 50 ) {
+            $animation_elements_left.animate({
            opacity: 0.25,
-           left: "-600px"
+           left: "-555px"
        },2000);
-       $('.right').animate({
+       $animation_elements_right.animate({
            opacity: 0.25,
-           right: "-600px"
+           right: "-550px"
        },2000);
+        } else {
+           $animation_elements_left.animate({
+           opacity: 1,
+           right: "0"
+       },2000);
+       $animation_elements_right.animate({
+           opacity: 1,
+           left: "0"
+       },2000);   
+            
+        }
+       
         
     });
    
-});
+});*/
+
+  //FADEOUT EFFECT AT manageIt page
+jQuery('document').ready(function($) {
+
+
+  var $window = $(window);
+  var animation_elements_left = $('.left');
+   var animation_elements_right = $('.right');
+  $window.scroll(function() {
+
+    $('.col-md-6').each(function() {
+      var offsetTop = $(this).offset().top,
+          scrollTop = $window.scrollTop(),
+          height = $window.height(),
+          start = offsetTop,
+          scrollTopToAnimate = scrollTop - start,
+          animate = (1 - scrollTopToAnimate / 100);
+
+          if(scrollTop >= start && animate > 0) {
+             $animation_elements_left.animate({
+                opacity: 0.25,
+                left: "-550px"
+                },2000);
+            $animation_elements_right.animate({
+                opacity: 0.25,
+                right: "-550px"
+            },2000);
+            
+          } else if(scrollTop <= start) {
+            $animation_elements_left.animate({
+                opacity: 1,
+                right: "0"
+                },2000);
+            $animation_elements_right.animate({
+                opacity: 1,
+                left: "0"
+                  },2000);
+            }
+            
+    });
+    
+  });
+  });
+
+ 
+
